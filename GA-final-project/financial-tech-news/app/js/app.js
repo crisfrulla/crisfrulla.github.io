5c6b11acc8d9bb9d88bafcda83461108774d20c4
crisfrulla <cris.frulla@gmail.com>
2016-09-02 16:32:05 -0400

    final project

--- GA-final-project/financial-tech-news/app/js/app.js
+++ GA-final-project/financial-tech-news/app/js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+$(document).ready(function() {
+    $("body").hide().fadeIn("fast");
+});
+
+
 // Routes
 var app = angular.module("App", ["ngRoute", "ui.bootstrap", "chart.js"]);
 app.config(function($routeProvider) {
@@ -29,11 +34,12 @@ app.controller('newsCtrl', function($scope, $http) {
 //exchange
 app.controller('xchangeCtrl', function($scope, $http) {
   $scope.loading = true;
-  $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDEUR%22%2C%22USDGBP%22%2C%22USDCAD%22%2C%22USDJPY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDEUR%22%2C%22USDCHF%22%2C%22USDCNY%22%2C%22USDBRL%22%2C%22USDAUD%22%2C%22USDGBP%22%2C%22USDCAD%22%2C%22USDJPY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
   .then(function (response) {
       $scope.rate = response.data.query.results.rate;
       $scope.loading = false;
       $scope.xchangeFilter = "USD/EUR";
+      $scope.create = response.data.query.created;
   });
 });
 
@@ -48,41 +54,34 @@ app.controller('financeNewsCtrl', function($scope, $http) {
 });
 
 // chartjs
-app.controller("LineCtrl", function ($scope, $http) {
-
+app.controller("LineCtrlApple", function ($scope, $http) {
   $scope.loading = true;
-  $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.historicaldata%20WHERE%20symbol%3D%22AAPL%22%20and%20startDate%3D%222016-08-01%22%20and%20endDate%3D%222016-08-26%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.historicaldata%20WHERE%20symbol%3D%22AAPL%22%20and%20startDate%3D%222016-08-29%22%20and%20endDate%3D%222016-09-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
   .then(function (response) {
-      //$scope.rate = query.results.quote.close;
-      //var date = response.data.query.results.quote[i].Date;
+      var quoteArr = response.data.query.results.quote;
       $scope.loading = false;
-      //console.log(response.data.query.results.quote[i].Date);
-        // var test = [];
-        //
-        // for (var d = 0; d < date.length; d++) {
-        //     test.push(d);
-        //
-        // }
-      console.log(response.data.query.results.quote[8].Date);
-
-      // date.map(function(n) {
-      //   $scope.labels.push(n.$scope.label);
-      // });
-      // console.log($scope.label);
-
-
-
-      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      $scope.series = ['Series A', 'Series B', 'Series C'];
-      $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [25, 39, 60, 91, 46, 35, 20],
-        [28, 48, 40, 19, 86, 27, 90]
-      ];
+      var dateArr = [];
+      var dataArr = [];
+      var openDataArr = [];
+      var closeDataArr = [];
+      var symbolArr = [];
+      quoteArr.forEach(function(quote){
+        dateArr.push(quote.Date)
+        //dataArr.push(Math.round(quote.Close))
+        dataArr.push(quote.Close)
+        closeDataArr.push(quote.Close)
+        openDataArr.push(quote.Open)
+        symbolArr.push(quote.Symbol)
+      });
+      $scope.openData = openDataArr;
+      $scope.closeData = closeDataArr;
+      $scope.labels = dateArr.reverse();
+      $scope.series = symbolArr;
+      $scope.data = [dataArr];
       $scope.onClick = function (points, evt) {
         console.log(points, evt);
       };
-      $scope.colours = ['#ff0000', '#00ff00', '#0000ff'];
+      $scope.colours = ['#08c'];
       $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
       $scope.options = {
         scales: {
@@ -103,6 +102,172 @@ app.controller("LineCtrl", function ($scope, $http) {
         }
       };
   });
-
-
+});
+app.controller("LineCtrlGoogle", function ($scope, $http) {
+  $scope.loading = true;
+  $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.historicaldata%20WHERE%20symbol%3D%22GOOGL%22%20and%20startDate%3D%222016-08-29%22%20and%20endDate%3D%222016-09-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  .then(function (response) {
+      var quoteArr = response.data.query.results.quote;
+      $scope.loading = false;
+      var dateArr = [];
+      var dataArr = [];
+      var openDataArr = [];
+      var closeDataArr = [];
+      var symbolArr = [];
+      quoteArr.forEach(function(quote){
+        dateArr.push(quote.Date)
+        //dataArr.push(Math.round(quote.Close))
+        dataArr.push(quote.Close)
+        closeDataArr.push(quote.Close)
+        openDataArr.push(quote.Open)
+        symbolArr.push(quote.Symbol)
+      });
+      $scope.openData = openDataArr;
+      $scope.closeData = closeDataArr;
+      $scope.labels = dateArr.reverse();
+      $scope.series = symbolArr;
+      $scope.data = [dataArr];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.colours = ['#ff0000'];
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      $scope.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+  });
+});
+app.controller("LineCtrlYahoo", function ($scope, $http) {
+  $scope.loading = true;
+  $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.historicaldata%20WHERE%20symbol%3D%22YHOO%22%20and%20startDate%3D%222016-08-29%22%20and%20endDate%3D%222016-09-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  .then(function (response) {
+      var quoteArr = response.data.query.results.quote;
+      $scope.loading = false;
+      var dateArr = [];
+      var dataArr = [];
+      var openDataArr = [];
+      var closeDataArr = [];
+      var symbolArr = [];
+      quoteArr.forEach(function(quote){
+        dateArr.push(quote.Date)
+        //dataArr.push(Math.round(quote.Close))
+        dataArr.push(quote.Close)
+        closeDataArr.push(quote.Close)
+        openDataArr.push(quote.Open)
+        symbolArr.push(quote.Symbol)
+      });
+      $scope.openData = openDataArr;
+      $scope.closeData = closeDataArr;
+      ;
+      $scope.labels = dateArr.reverse();
+      $scope.series = symbolArr;
+      $scope.data = [dataArr];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.colours = ['#020e65'];
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      $scope.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+  });
+});
+app.controller("LineCtrlAmazon", function ($scope, $http) {
+  $scope.loading = true;
+  $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.historicaldata%20WHERE%20symbol%3D%22AMZN%22%20and%20startDate%3D%222016-08-29%22%20and%20endDate%3D%222016-09-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  .then(function (response) {
+      var quoteArr = response.data.query.results.quote;
+      $scope.loading = false;
+      var dateArr = [];
+      var dataArr = [];
+      var openDataArr = [];
+      var closeDataArr = [];
+      var symbolArr = [];
+      quoteArr.forEach(function(quote){
+        dateArr.push(quote.Date)
+        //dataArr.push(Math.round(quote.Close))
+        dataArr.push(quote.Close)
+        closeDataArr.push(quote.Close)
+        openDataArr.push(quote.Open)
+        symbolArr.push(quote.Symbol)
+      });
+      $scope.openData = openDataArr;
+      $scope.closeData = closeDataArr;
+      ;
+      $scope.labels = dateArr.reverse();
+      $scope.series = symbolArr;
+      $scope.data = [dataArr];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.colours = ['#f08804'];
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      $scope.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+  });
+  // Slick
+  $('.stock-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: true
+  });
+});
+// Typed
+$("h1").typed({
+    strings: ["ANALYZE", "STRATEGIZE", "CAPITALIZE",  "FIN / TECH "],
+    typeSpeed: 0,
+    showCursor: false
+});
+$("#tagline").typed({
+    strings: ["Get the trading platform you need to understand past trends and foresee profitable trading opportunities"],
+    typeSpeed: 0,
+    startDelay: 5900,
+    showCursor: false
 });
